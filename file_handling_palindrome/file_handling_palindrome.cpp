4d4e853fdb53a5b965d33485f82a81514563dd3e
janny801 <jred8069@gmail.com>
2025-07-10 13:28:01 -0500

    add program

---
+++ file_handling_palindrome/file_handling_palindrome.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+bool isPalindrome(string x)
+{
+
+    //get the reversed string (to compare to original)
+    string reverse = ""; 
+    for(int i =x.length()-1; i>=0; i--)
+    {
+        //
+        reverse+=x[i]; 
+    }
+
+
+    // cout<<"regular: "<<x<<endl; 
+    // cout<<"reversed: "<<reverse<<endl; 
+
+    //compare char by char the regular and reverse 
+    for(int i = 0; i<x.length(); i++)
+    {
+        if(x[i]!=reverse[i])
+        {
+            return false; 
+        }
+    }
+    return true; 
+
+
+}
+
+ 
+int main()
+{
+    
+    // Enter your code here
+    fstream input; 
+    input.open("input.txt"); 
+
+    //go thru each iteration and pass each line to the isPalindrome func
+    string line; 
+    while(getline(input,line))
+    {
+
+
+        //remove carriage return (edge cases) 
+        if(!line.empty() && line.back() == '\r')
+        {
+            line.pop_back(); 
+        }
+
+        //pass to isPalindrome func
+        if(isPalindrome(line))
+        {
+            //isPalindrome is true
+            cout<<"Palindrome"<<endl; 
+        }
+        else
+        {
+            //isPalindrome is false
+            cout<<"Not a palindrome"<<endl; 
+        }
+    }
+
+    return 0;
+}
